0b7e8dc180b4f2149b7bfe5b9c4e7a38dfbec083
Naftuli Kay <me@naftuli.wtf>
2017-09-05 21:23:35 -0700

    A Bad Stack - 2.6: Testing

--- src/first.rs
+++ src/first.rs
@@ -39,3 +39,34 @@ struct Node {
     elem: i32,
     next: Link,
 }
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn push_and_pop() {
+        let mut list = List::new();
+
+        // if empty, return none
+        assert_eq!(list.pop(), None);
+
+        // populate
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        // check that appending again works
+        list.push(4);
+        list.push(5);
+
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None)
+    }
+}
