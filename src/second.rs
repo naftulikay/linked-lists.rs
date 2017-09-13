0700f03d6260810e1c8ef55aa7f42c47915392b8
Naftuli Kay <me@naftuli.wtf>
2017-09-13 16:29:24 -0700

    An Ok Stack - 3.1 Option

---
+++ src/second.rs
@@ -0,0 +1,73 @@
+pub struct List {
+    head: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
+        List { head: None }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: self.head.take(),
+        });
+
+        self.head = Some(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        self.head.take().map(|node| {
+            let node = *node;
+            self.head = node.next;
+            node.elem
+        })
+    }
+}
+
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut current_link = self.head.take();
+        while let Some(mut boxed_node) = current_link {
+            current_link = boxed_node.next.take();
+        }
+    }
+}
+
+type Link = Option<Box<Node>>;
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn push_and_pop() {
+        let mut list = List::new();
+
+        // if empty, return none
+        assert_eq!(list.pop(), None);
+
+        // populate
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        // check that appending again works
+        list.push(4);
+        list.push(5);
+
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None)
+    }
+}
